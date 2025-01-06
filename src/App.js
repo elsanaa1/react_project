e26a94c57c13402bba97ef7885f5f90f2faf0c5e
elsanaa1 <sanaa.jerbouh1@gmail.com>
2025-01-06 21:11:47 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navigation from './NavBar';
+
+
+
+const App = () =>{
+  return (
+    <>
+    <Navigation />
+    <div>hello</div> 
+    </>  
+  );
+}
+
+export default App;
+
+
---
+++ src/NavBar.js
@@ -0,0 +1,15 @@
+import Navbar from 'react-bootstrap/Navbar';
+
+const Navigation = () => {
+
+    let name = "react"
+      return (
+       <>
+      
+        <Navbar.Brand href="#home" style={{color:"red"}}>React-Bootstrap</Navbar.Brand>
+        </>
+    );
+}
+
+
+export default Navigation;
